f0e114c1c2834133bc9e5f594586de1525ee6bd7
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-12-15 21:34:56 +0300

    fix info button

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -30,7 +30,7 @@ const renderArticle = ({ title, link, description }) => {
   <li class="list-group-item d-flex justify-content-between align-items-center bg-light">
     <a href=${link} class="text-dark">${title}</a>
     <button type="button" class="btn btn-secondary ml-4 button-info" data-toggle="modal" data-target="#info-modal" data-whatever='${description}'>
-      Инфо
+      Информация
     </button>
   </li>
   `;
